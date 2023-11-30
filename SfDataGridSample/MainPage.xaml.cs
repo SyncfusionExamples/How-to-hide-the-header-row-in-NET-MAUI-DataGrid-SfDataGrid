16265c3088bd8cbad7cb241d0998695fdf3a17fc
Sudarsan Muthuselvan <sudarsan.m@syncfusion.com>
2023-11-30 15:24:23 +0530

    sample added

---
+++ SfDataGridSample/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Syncfusion.Maui.DataGrid;
+
+namespace SfDataGridSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void datagrid_CellLongPress(object sender, DataGridCellLongPressEventArgs e)
+        {
+            viewModel.Employees.Remove((Employee)e.RowData);
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
